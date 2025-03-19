3baab04b81265557691f451c938c4535975ccb3f
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2025-03-19 21:14:00 +0700

    change index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,5 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href="/login">Login</Link>
-      <Link href="/produk">Produk</Link>
-      <Link href="/transaksi">Transaksi</Link>
-      <Link href="/customer">Customer</Link>
-    </View>
-  );
+  return <Redirect href={"/login"} />
 }
